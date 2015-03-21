43d2c089a863fb00d24463a858e1a0f0d787fe25
akashruti <shruti_01@live.com>
2015-03-20 23:52:49 -0400

    Added

---
+++ Assignment4/Assignment4-Part1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random Number Generator *********************
+            Random rnd = new Random();
+
+            //Variable Declaration and Initialization **************
+            int firstRoll = 0;
+            int secondRoll = 0;
+            int sum;
+
+            //Array Declaration ******************
+            int[] sumArray = new int[11];
+
+
+            //for loop to roll the dice 36000 times
+            for (int i = 0; i < 36000; i++)
+            {
+                firstRoll = rnd.Next(1, 7);
+                secondRoll = rnd.Next(1, 7);
+                sum = firstRoll + secondRoll;
+
+                //switch statement to add up how many times each value is achieved
+                switch (sum)
+                {
+                    case 2:
+                        sumArray[0]++;
+                        break;
+                    case 3:
+                        sumArray[1]++;
+                        break;
+                    case 4:
+                        sumArray[2]++;
+                        break;
+                    case 5:
+                        sumArray[3]++;
+                        break;
+                    case 6:
+                        sumArray[4]++;
+                        break;
+                    case 7:
+                        sumArray[5]++;
+                        break;
+                    case 8:
+                        sumArray[6]++;
+                        break;
+                    case 9:
+                        sumArray[7]++;
+                        break;
+                    case 10:
+                        sumArray[8]++;
+                        break;
+                    case 11:
+                        sumArray[9]++;
+                        break;
+                    case 12:
+                        sumArray[10]++;
+                        break;
+                }
+            }
+
+
+            //Displaying to console
+            Console.WriteLine();
+            Console.WriteLine("\t***********************************");
+            Console.WriteLine("\t\t**** TOTAL ****");
+            Console.WriteLine("\t***********************************");
+            Console.WriteLine();
+            //For loop to go through each index in the array and display its value
+            Console.WriteLine("\t    Faces    Frequency");
+            for (int index = 0; index < sumArray.Length; index++)
+            {
+                Console.WriteLine("\t {0,5}: {1,10} ", index + 2, sumArray[index]);
+            }
+
+            WaitForKey();
+        }
+        private static void WaitForKey()
+        {
+            Console.WriteLine();
+            Console.WriteLine("\t***********************************");
+            Console.WriteLine("\t    Press any key to continue...");
+            Console.WriteLine("\t***********************************");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment4/Assignment4/Program.cs
@@ -0,0 +1,224 @@
+﻿/*
+*
+* Course: COMP-123-Assignment-04
+* Section: 062
+* Student Name: Shruti Kirti Kaushal
+* Student ID: 300804023
+ * 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Program
+    {
+        // instance variables+++++++++++++++++++++++++++++++++++++
+        public static bool[] seats ={false,false,false,false,false,
+                                   false,false,false,false,false};//total 10seats are empty
+        //public static bool[] seats;
+
+
+        public static int totalFirstClassSeats;
+        public static int totalEconomyClassSeats;
+
+
+        static void Main(string[] args)
+        {
+            MainMenu(); //Call the Menu method
+
+        }
+
+        //Private Menu Method+++++++++++++++++++++++++++++++++++++++++++++++++++++
+        private static void MainMenu()
+        {
+            int selection = 0;
+
+            while (selection != 3)
+            {
+                Console.WriteLine("+++++++++++++++++++++++++++++++");
+                Console.WriteLine("+ Airline_Reservations_System +");
+                Console.WriteLine("+                             +");
+                Console.WriteLine("+      1. First Class         +");
+                Console.WriteLine("+      2. Economy Class       +");
+                Console.WriteLine("+      3. Exit                +");
+                Console.WriteLine("+                             +");
+                Console.WriteLine("+++++++++++++++++++++++++++++++");
+                Console.Write("Please make your selection: ");
+
+                //To listen and handle Exceptions
+
+                try
+                {
+                    selection = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception error)
+                {
+                    selection = 0;
+                    Console.WriteLine(error.Message);
+                }
+
+                switch (selection)
+                {
+                    case 1:
+                        ConditionalFirstClass();
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        break;
+                    case 2:
+                        ConditionalEconomyClass();
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        Console.WriteLine();
+                        break;
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Incorrect entry - Please try again");
+                        Console.WriteLine();
+                        WaitForKey();
+                        break;
+                }
+
+                
+            }
+            
+            
+        }
+       
+
+        //Private Conditional Method+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //asks the customer if like to change to economy class when first class is full
+        private static void ConditionalFirstClass()
+        {
+            string choose = "";
+            if (totalFirstClassSeats == 5 && totalEconomyClassSeats < 5)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Sorry, first class is full! ");
+                Console.WriteLine("Would like to change to economy class?");
+                Console.Write("Press Y to change or N to the menu: ");
+
+                choose = Console.ReadLine();
+                if (choose == "N")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Next flight leaves in 3 hours.");
+                }
+                else if (choose == "Y")
+                {
+                    Console.WriteLine();
+                    EconomyClassSection();
+                }
+
+            }
+            else if (totalEconomyClassSeats == 5 && totalFirstClassSeats == 5)
+            {
+                Console.WriteLine("Sorry all seats are full!");
+                Console.WriteLine();
+            }
+            else if (totalFirstClassSeats < 5)
+            {
+                Console.WriteLine();
+                FirstClassSection();
+            }
+        }
+
+        //asks the customer if they would like to change to first class provided economy class is full
+        private static void ConditionalEconomyClass()
+        {
+            string choose = "";
+            if (totalEconomyClassSeats == 5 && totalFirstClassSeats < 5)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Sorry, economy class is full! ");
+                Console.WriteLine("Would like to change to first class?");
+                Console.Write("Press Y to change or N to the menu: ");
+
+                choose = Console.ReadLine();
+                if (choose == "N")
+                {
+                    Console.WriteLine("Next flight leaves in 3 hours.");
+                }
+                else if (choose == "Y")
+                {
+                    Console.WriteLine();
+                    FirstClassSection();
+                }
+
+            }
+            else if (totalEconomyClassSeats == 5 && totalFirstClassSeats == 5)
+            {
+                Console.WriteLine("Sorry all seats are full!");
+                Console.WriteLine();
+            }
+            else if (totalEconomyClassSeats < 5)
+            {
+                Console.WriteLine();
+                EconomyClassSection();
+            }
+
+        }
+
+        //Private First class method++++++++++++++++++++++++++++++++++++++++++
+        private static void FirstClassSection()
+        {
+            bool assignedSeat = false; //indicate the seat is aready assigned
+            Random rand = new Random();
+            int randomSeatNumber = 0;
+
+            while (!assignedSeat)
+            {
+                assignedSeat = true;
+                randomSeatNumber = rand.Next(1, 6);
+                if (seats[randomSeatNumber] == true)
+                {
+                    assignedSeat = false;
+                }
+
+            }
+            seats[randomSeatNumber] = true; //seat change to be assigned
+            totalFirstClassSeats++;
+            Console.WriteLine("Your seat number is {0} in first class", randomSeatNumber);
+        }
+
+        //Private Economy class method++++++++++++++++++++++++++++++++++++++++++
+        private static void EconomyClassSection()
+        {
+            bool assignedSeat = false; //indicate the seat is aready assigned
+            Random rand = new Random();
+            int randomSeatNumber = 0;
+
+            //keep looping until the empty seat has been found
+            while (!assignedSeat)
+            {
+                assignedSeat = true;
+                randomSeatNumber = rand.Next(6, 11);
+                if (seats[randomSeatNumber] == true)
+                {
+                    assignedSeat = true;
+                }
+
+            }
+            seats[randomSeatNumber] = true; //seat change to be occupied
+            totalEconomyClassSeats++;
+            Console.WriteLine("Your seat is {0} in economy class", randomSeatNumber);
+        }
+
+        // UTILITY METHODS+++++++++++++++++++++++++++++++++++++++++++++++++++++
+        private static void WaitForKey()
+        {
+            Console.WriteLine();
+            Console.WriteLine("++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
+
